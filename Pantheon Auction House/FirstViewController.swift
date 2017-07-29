1c46c09d717756a77eca3a830028e1cd9260bf26
Matthew Cooper <mcooper@servpronet.com>
2017-07-28 21:33:04 -0700

    Initial Commit

---
+++ Pantheon Auction House/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pantheon Auction House
+//
+//  Created by Matthew Cooper on 7/28/17.
+//  Copyright © 2017 Matthew Cooper. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
